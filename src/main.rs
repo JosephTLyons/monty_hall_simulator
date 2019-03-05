3eaa833efc1281f8b2d17f33aab50eaa25d100ec
Joseph Lyons <josephtlyons@gmail.com>
2019-03-05 15:39:24 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ use std::io;
 
 fn main() {
     let mut wins = 0;
-    let total_runs = 3;
+    let total_runs = 100;
     for _x in 0..total_runs {
-        if monty_hall(true) {
+        if monty_hall (true) {
             wins += 1;
         }
     }
@@ -18,35 +18,22 @@ fn main() {
 fn monty_hall(swap: bool) -> bool {
     let mut doors: Vec<bool> = vec![false; 3];
     let correct_door: usize = rand::thread_rng().gen_range (0, 3);
-
     doors[correct_door] = true;
-    // dbg!(&doors);
 
-    println!("Guess a door: ");
+    let mut door_chosen: usize = rand::thread_rng().gen_range (0, 3);
+    println!("Door chosen: {}", door_chosen);
 
-    let mut input: String = String::new();
-    io::stdin().read_line(&mut input).expect ("Couldn't read input.");
+    if guess_is_in_range(door_chosen, doors.len()) {
+        let wrong_door_revealed: usize = get_index_of_first_wrong_door(&doors, door_chosen);
 
-    let mut guess: usize = input.trim().parse::<usize>().expect("Couldn't parse string to usize");
-
-    if guess_is_in_range(guess, doors.len()) {
-        let wrong_door_revealed: usize = get_index_of_first_wrong_door(&doors, guess);
-        println!("door {} is not the winner!", wrong_door_revealed);
-        println!("Would you like to swap? (y/n)");
-        input.clear();
-        io::stdin().read_line(&mut input).expect ("Couldn't read input.");
-        let swap_choice: char = input.trim().parse::<char>().expect("Couldn't parse string to char");
-
-        dbg!(swap_choice);
-
-        // if swap_choice == 'y' {
         if swap {
+            println!("You've swapped doors");
             // Add up indices
-            let remaining_option = (0 + 1 + 2) - (guess + wrong_door_revealed);
-            guess = remaining_option;
+            let remaining_option = (1 + 2) - (door_chosen + wrong_door_revealed);
+            door_chosen = remaining_option;
         }
 
-        if guess_is_correct(&doors, guess) {
+        if guess_is_correct(&doors, door_chosen) {
             game_won();
             return true;
         }
