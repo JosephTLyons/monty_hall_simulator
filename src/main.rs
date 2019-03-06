5680c6065985123a2e4b8e7b705afdfe607f289c
Joseph Lyons <josephtlyons@gmail.com>
2019-03-05 19:40:47 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate rand;
 use rand::Rng;
 
 fn main() {
-    monty_hall_driver(10000);
+    monty_hall_driver(100_000);
 }
 
 fn monty_hall_driver(run_count: u32) {
@@ -19,16 +19,21 @@ fn monty_hall_driver(run_count: u32) {
         println!();
     }
 
-    println!("You won: {}/{}", wins, run_count);
+    println!(
+        "You won: {}/{} ({}%)",
+        wins,
+        run_count,
+        (wins as f32 / run_count as f32) * 100.0
+    );
 }
 
 fn monty_hall(swap: bool) -> bool {
     let mut doors: Vec<bool> = vec![false; 3];
-    let correct_door: usize = rand::thread_rng().gen_range (0, 3);
+    let correct_door: usize = rand::thread_rng().gen_range(0, 3);
     doors[correct_door] = true;
     println!("Correct door is: {}", correct_door);
 
-    let mut door_chosen: usize = rand::thread_rng().gen_range (0, 3);
+    let mut door_chosen: usize = rand::thread_rng().gen_range(0, 3);
     println!("Door chosen: {}", door_chosen);
 
     let wrong_door_revealed: usize = get_index_of_first_wrong_door(&doors, door_chosen);
