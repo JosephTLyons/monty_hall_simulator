af2eea962ce5edbefad6c676ff6b44ee0a6e490d
Joseph Lyons <josephtlyons@gmail.com>
2019-03-18 00:26:04 -0400

    Renamed variable to make it more readable

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ fn main() {
     monty_hall_driver(1000, true);
 }
 
-fn monty_hall_driver(run_count: u32, swap: bool) {
+fn monty_hall_driver(run_count: u32, should_swap: bool) {
     let mut wins: u32 = 0;
 
     for x in 0..run_count {
         println!("Run: {}", x + 1);
 
-        if monty_hall(swap) {
+        if monty_hall(should_swap) {
             wins += 1;
         }
 
@@ -27,7 +27,7 @@ fn monty_hall_driver(run_count: u32, swap: bool) {
     );
 }
 
-fn monty_hall(swap: bool) -> bool {
+fn monty_hall(should_swap: bool) -> bool {
     let mut doors: Vec<bool> = vec![false; 3];
     let correct_door: usize = rand::thread_rng().gen_range(0, 3);
     doors[correct_door] = true;
@@ -39,7 +39,7 @@ fn monty_hall(swap: bool) -> bool {
     let wrong_door_revealed: usize = get_index_of_first_wrong_door(&doors, door_chosen);
     println!("Revealed wrong door: {}", wrong_door_revealed);
 
-    if swap {
+    if should_swap {
         let remaining_door = 3 - (door_chosen + wrong_door_revealed);
         println!("You've swapped from {} to {}", door_chosen, remaining_door);
         door_chosen = remaining_door;
