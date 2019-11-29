60e89d3c25ed682f58a3ecaa5849265f3aab3f4c
Joseph Lyons <josephtlyons@gmail.com>
2019-11-29 16:37:41 -0500

    Some code cleaning

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ fn main() {
 fn monty_hall_driver(run_count: u32, should_swap: bool) {
     let mut wins: u32 = 0;
 
-    for x in 0..run_count {
-        println!("Run: {}", x + 1);
+    for i in 0..run_count {
+        println!("Run: {}", i + 1);
 
         if monty_hall(should_swap) {
             wins += 1;
@@ -26,36 +26,34 @@ fn monty_hall_driver(run_count: u32, should_swap: bool) {
 }
 
 fn monty_hall(should_swap: bool) -> bool {
-    let mut doors: [bool; 3] = [false; 3];
-    let correct_door: usize = rand::thread_rng().gen_range(0, 3);
+    const NUMBER_OF_DOORS: usize = 3;
+    let mut doors: [bool; NUMBER_OF_DOORS] = [false; NUMBER_OF_DOORS];
+    let correct_door: usize = rand::thread_rng().gen_range(0, NUMBER_OF_DOORS);
+
     doors[correct_door] = true;
     println!("Correct door: {}", correct_door);
 
-    let mut door_chosen: usize = rand::thread_rng().gen_range(0, 3);
+    let mut door_chosen: usize = rand::thread_rng().gen_range(0, NUMBER_OF_DOORS);
     println!("Door chosen: {}", door_chosen);
 
     let wrong_door_revealed: usize = get_index_of_first_wrong_door(&doors, door_chosen);
     println!("Revealed wrong door: {}", wrong_door_revealed);
 
     if should_swap {
-        let remaining_door = 3 - (door_chosen + wrong_door_revealed);
+        let remaining_door = NUMBER_OF_DOORS - (door_chosen + wrong_door_revealed);
         println!("You've swapped from {} to {}", door_chosen, remaining_door);
         door_chosen = remaining_door;
-    }
-
-    else {
+    } else {
         println!("You've kept door {}", door_chosen);
     }
 
     if doors[door_chosen] {
         println!("You won!");
-        true
+        return true;
     }
 
-    else {
-        println!("You lost!");
-        false
-    }
+    println!("You lost!");
+    false
 }
 
 fn get_index_of_first_wrong_door(doors: &[bool], guess: usize) -> usize {
